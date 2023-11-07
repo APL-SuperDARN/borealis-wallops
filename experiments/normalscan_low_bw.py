6aa198ca8164a75f7acac28dededbab2a092f878
Jordan Wiker <jordan.wiker@jhuapl.edu>
2023-11-07 16:57:25 -0500

    Create low-bandwidth normalscan

---
+++ experiments/normalscan_low_bw.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+# write an experiment that creates a new control program.
+
+import sys
+import os
+
+BOREALISPATH = os.environ['BOREALISPATH']
+sys.path.append(BOREALISPATH)
+
+import experiments.superdarn_common_fields as scf
+from experiment_prototype.experiment_prototype import ExperimentPrototype
+
+
+class Normalscan(ExperimentPrototype):
+
+    def __init__(self, **kwargs):
+        """
+        kwargs:
+
+        freq: int
+
+        """
+        cpid = 151
+        output_rx_rate = 10.0e3/3
+        tx_bandwidth = 0.5e6
+        rx_bandwidth = 0.5e6
+        super(Normalscan, self).__init__(cpid, output_rx_rate, rx_bandwidth, tx_bandwidth)
+
+        if scf.IS_FORWARD_RADAR:
+            beams_to_use = scf.STD_24_FORWARD_BEAM_ORDER
+        else:
+            beams_to_use = scf.STD_24_REVERSE_BEAM_ORDER
+
+        num_ranges = scf.STD_NUM_RANGES
+
+        # default frequency set here
+        freq = scf.COMMON_MODE_FREQ_1
+
+        if kwargs:
+            if 'freq' in kwargs.keys():
+                freq = kwargs['freq']
+
+        self.printing('Frequency set to {}'.format(freq))
+
+        self.add_slice({  # slice_id = 0, there is only one slice.
+            "pulse_sequence": scf.SEQUENCE_7P,
+            "tau_spacing": scf.TAU_SPACING_7P,
+            "pulse_len": scf.PULSE_LEN_45KM,
+            "num_ranges": num_ranges,
+            "first_range": scf.STD_FIRST_RANGE,
+            "intt": scf.INTT_7P_24,  # duration of an integration, in ms
+            "beam_angle": scf.STD_24_BEAM_ANGLE,
+            "rx_beam_order": beams_to_use,
+            "tx_beam_order": beams_to_use,
+            "scanbound": scf.easy_scanbound(scf.INTT_7P_24, beams_to_use), #1 min scan
+            "freq" : freq, #kHz
+            "acf": True,
+            "xcf": True,  # cross-correlation processing
+            "acfint": True,  # interferometer acfs
+            "wait_for_first_scanbound": False,
+        })
